0587d3fccfe098ba899fd6484bc6a7625fc66f73
YiFei Zhu <zhuyifei1999@gmail.com>
2021-10-01 11:00:17 -0700

    bwrap.sh: Add some ulimits

--- bwrap.sh
+++ bwrap.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ulimit -Sc 0
+ulimit -v 4194304
+ulimit -t 600
+ulimit -f 8129
+ulimit -n 128
+ulimit -u 8192
+ulimit -Ss 8192
+ulimit -l 64
+ulimit -r 0
+ulimit -q 1024
+
 stty cols 80 rows 24
 exec env -i TERM=linux COLUMNS=80 LINES=24 HOME="$HOME" \
   bwrap --ro-bind / / \
-    --ro-bind /tmp/socket /tmp/socket \
     --dir /tmp \
     --proc /proc \
     --dev /dev \
@@ -17,4 +27,4 @@ exec env -i TERM=linux COLUMNS=80 LINES=24 HOME="$HOME" \
     --hostname osaibot \
     --die-with-parent \
     --as-pid-1 \
-    ~/osai-bot/bash/bash -l
+    bash -l
